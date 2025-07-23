6bf59319d0ddcb1645d99dbc173f94e737d91d3d
silian <13144258512@163.com>
2025-07-23 14:08:58 +0800

    更新 .gitignore 并移除忽略文件的缓存

--- .umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'E:/之前桌面的文件/2024重新学习/工程化/monorepro-project-test/node_modules/.pnpm/@umijs+runtime@3.5.43_react@18.2.0/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
